75364daaa838d26b4e01bf671f83b20a675c00cd
superfrank <frank.hulley-jones@guardian.co.uk>
2019-12-02 22:45:39 +0000

    fixed for the evening

--- scripts/compile.js
+++ scripts/compile.js
@@ -10,19 +10,18 @@ const template = handlebars.compile(source);
 handlebars.registerHelper("inc", index => index++);
 
 // <<from here
-handlebars.registerPartial("head", "{{head}}");
-handlebars.registerPartial("header", "{{header}}");
-handlebars.registerPartial("footer", "{{footer}}");
-const output = template(data);
+
 // console.log(output);
 
-let partials = glob.sync("src/templates/**/*.*");
+let partials = glob.sync("src/templates/includes/*.*");
 // console.log(partials);
 
 partials.forEach(function(partial) {
-  var name = partial.replace("src/templates/", "").split(".")[0];
+  var name = partial.replace("src/templates/includes/", "").split(".")[0];
   var template = fs.readFileSync(partial, "utf8");
 
   handlebars.registerPartial(name, template);
 });
-console.log(partials);
+
+const output = template(data);
+console.log(output);
